66adc62b67b0dee7703737bc251f3534c4b0d28a
SherlockCheng <sherlockcheng0629@gmail.com>
2026-07-16 22:03:31 +0800

    Add new project

---
+++ Projects/StudentManager/student.cpp
@@ -0,0 +1,22 @@
+﻿#include "student.h"
+#include <iostream>
+#include <string>
+#include <map>
+
+class Student
+{
+public:
+	Student();
+	~Student();
+
+private:
+	std::string name;
+	int id;
+	std::map<std::string, double> scores;
+};
+
+Student::Student()
+{}
+
+Student::~Student()
+{}
